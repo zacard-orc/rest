245e53c5951925fd7445a580fda0d701c5ac0a0c
zarcard-orc <13816025562@139.com>
2014-10-14 13:32:17 +0800

    async lab 2

--- asyc/asy002.js
+++ asyc/asy002.js
@@ -5,3 +5,7 @@
 var as_x=require('async');
 var moment=require('moment');
 var log=require('./t').log;
+
+var arr = [{name:'Jack', delay:200}, {name:'Mike', delay: 100}, {name:'Freewind', delay:300}, {name:'Test', delay: 50}];
+
+
