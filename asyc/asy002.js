2f4ed94e5910b3f6f15c43ac92d0fa676065052e
zarcard-orc <13816025562@139.com>
2014-10-14 15:11:26 +0800

    async lab 2

--- asyc/asy002.js
+++ asyc/asy002.js
@@ -6,10 +6,73 @@ var as_x=require('async');
 var moment=require('moment');
 var log=require('./t').log;
 
-var arr = [{name:'Jack', delay:200},
-           {name:'Mike', delay: 100},
-           {name:'Freewind', delay:300},
-           {name:'Test', delay: 50}];
+var arr = [{name:'Jack', delay:2000},
+           {name:'Mike', delay: 1000},
+           {name:'Freewind', delay:3000},
+           {name:'Test', delay: 500}];
+
+var f_col={
+    'f_a':function(a,b){console.log(a+b);return a+b;},
+    'f_b':function(a,c){console.log(a-c);return a-c;},
+    'f_c':function(a,d){console.log(a+d);return a/d;},
+    'f_d':function(a,e){console.log(a*e);return a*e;}
+}
+
+var f_par_x={
+    'f_a':10,
+    'f_b':20,
+    'f_c':30,
+    'f_d':40
+}
+
+var f_par_y={
+    'f_a':1,
+    'f_b':2,
+    'f_c':3,
+    'f_d':4
+}
+
+var tasks=['f_b','f_a','f_c','f_d'];
+
+log('1.1 starts: ');
+/*
+as_x.map(tasks,function(item,callback){
+        //f_col[item](f_par_x[item],f_par_y[item]);
+        setTimeout(function()
+                  {callback(null,f_col[item](f_par_x[item],f_par_y[item]));}
+                  ,1000);
+},function(err,results){
+     log('1.1 results: ', results);
+});
+
+
+as_x.map(tasks, function(item, callback) {
+    if (item=='f_c') callback('myerr');
+    else callback(null,f_col[item](f_par_x[item],f_par_y[item]));
+}, function(err, results) {
+    log('1.2 err: ', err);
+    log('1.2 results: ', results);
+});
+
+as_x.mapSeries(tasks,function(item,callback){
+    //f_col[item](f_par_x[item],f_par_y[item]);
+    setTimeout(function()
+        {callback(null,f_col[item](f_par_x[item],f_par_y[item]));}
+        ,1000);
+},function(err,results){
+    log('1.1 results: ', results);
+});
+
+ */
+
+//as_x.mapLimit() 单批最多执行两个程序
+
+/*
+ 18
+ 11
+ 04.266> 1.2 results: [ 18, 11, undefined ]
+ 160
+ */
 
 /* 对集合中的每一个元素，执行某个异步操作，得到结果。
   所有的结果将汇总到最终的callback里。与each的区别是，
@@ -21,7 +84,7 @@ var arr = [{name:'Jack', delay:200},
  * 2. 顺序执行。对集合中的元素一个一个执行操作，结果汇总到最终callback里。
  *    如果出错，则立刻返回错误以及已经执行完的结果，未执行的被忽略。
  */
-
+/*
 as_x.map(arr, function(item, callback) {
     log('1.1 enter: ' + item.name);
     setTimeout(function() {
@@ -32,5 +95,9 @@ as_x.map(arr, function(item, callback) {
     log('1.1 err: ', err);
     log('1.1 results: ', results);
 });
+*/
+
+
+
 
 
