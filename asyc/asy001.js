70ace7cadd451892acd8a7ad01ada087cd790380
zarcard-orc <13816025562@139.com>
2014-10-14 10:29:30 +0800

    multi function map multi parameters

--- asyc/asy001.js
+++ asyc/asy001.js
@@ -20,10 +20,46 @@ console.log(now.add(7,'days').subtract(1,'hours').format('YYYYMMDD hh:mm:ss:SSS'
  20141020 09:05:20:737
  */
 
-/* 3件事情并行执行，没有异常产生 */
-var arr=[{name:'Jack', delay: 2000},
-         {name:'Mike', delay: 1000},
-         {name:'Freewind', delay: 3000}];
+
+
+var f_col={
+    'f_a':function(a,b){console.log(a+b);},
+    'f_b':function(a,c){console.log(a-c);},
+    'f_c':function(a,d){console.log(a+d);},
+    'f_d':function(a,e){console.log(a*e);}
+}
+
+var f_par_x={
+    'f_a':100,
+    'f_b':200,
+    'f_c':300,
+    'f_d':400
+}
+
+var f_par_y={
+    'f_a':1100,
+    'f_b':1200,
+    'f_c':1300,
+    'f_d':1400
+}
+
+var tasks=['f_b','f_a','f_c','f_d'];
+as_x.each(tasks,function(item,callback){
+    //console.log(item);
+    f_col[item](f_par_x[item],f_par_y[item]);
+    },
+    function(err){
+        console.log('err:'+err);
+    });
+
+
+
+/* 3件事情并行执行，没有异常产生，根据延时先后返回 */
+/*
+var arr=[{name:'Jack', delay: 2000, num:10},
+         {name:'Mike', delay: 1000, num:20},
+         {name:'Freewind', delay: 3000, num:30}];
+
 
 as_x.each(arr,function(item,callback){
     log('1.1 enter:'+item.name);
@@ -34,4 +70,4 @@ as_x.each(arr,function(item,callback){
 },function(err){
     log('1.1 err:'+err);
 });
-
+*/
