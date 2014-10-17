642240368b2a2ffe264bbbf27acd19028354e00f
zarcard-orc <13816025562@139.com>
2014-10-17 17:29:02 +0800

    asy004

--- asyc/asy004.js
+++ asyc/asy004.js
@@ -22,7 +22,7 @@ var log=require('./t').log;
 var q = as_x.queue(function(task, callback) {
     log('worker is processing task: ', task.name);
     task.run(callback);
-}, 2);
+}, 1);
 
 /**
  * 监听：如果某次push操作后，任务数将达到或超过worker数量时，将调用该函数
@@ -50,7 +50,7 @@ q.drain = function() {
  */
 q.push({name:'t1', run: function(cb){
     log('t1 is running, waiting tasks: ', q.length());
-    t.fire('t1', cb, 400); // 400ms后执行
+    t.fire('t1', cb, 5000); // 400ms后执行
 }}, function(err) {
     log('t1 executed');
 });
@@ -58,7 +58,7 @@ log('pushed t1, waiting tasks: ', q.length());
 
 q.push({name:'t2',run: function(cb){
     log('t2 is running, waiting tasks: ', q.length());
-    t.fire('t2', cb, 200); // 200ms后执行
+    t.fire('t2', cb, 10000); // 200ms后执行
 }}, function(err) {
     log('t2 executed');
 });
@@ -77,6 +77,7 @@ log('pushed t2, waiting tasks: ', q.length());
 
 
 // 同时加入多个任务
+/*
 q.push([
     {
         name:'t3', run: function(cb){
@@ -103,3 +104,4 @@ q.push([
     log('err: ',err);
 });
 log('pushed t3,t4,t5,t6 into queue, waiting tasks: ', q.length());
+    */
