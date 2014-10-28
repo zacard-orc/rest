66ee7bc67afce8acedfb650b4477b1fd2bc68c8a
zarcard-orc <13816025562@139.com>
2014-10-28 11:13:06 +0800

    json body

--- app3.js
+++ app3.js
@@ -1,33 +1,39 @@
-/**
- * Created by linly on 14-10-28.
- */
-
-
-//express@3.4.8 node_modules\express.
-    
-var express  = require('express');
-var http = require('http');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var fs = require('fs');
+var express = require('express');
 
 var app = express();
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+jspp=function(req,cb)
+{
+    var qss=''
+    req.on('data',function(chunk)
+    {
+        qss+=chunk;
+    });
+    req.on('end',function(){
+        cb(qss);
+    });
+};
 
-app.post('/2002',function(req,res){
-    console.log(req.path);
-    //console.log(req);
-    res.set({'Content-Type':'text/json','Encodeing':'utf8'});
-    res.send({msg:'SUCCESS'});
+
+//处理POST请求
+//name和email是POST请求域中的参数名
+
+app.post('/2002',function(req,res) {
+   jspp(req,function(body){
+       console.log(body);
+       res.send('Post Over');
+   });
 });
 
-app.listen(5555,function(){
-    console.log('RestFul Web is Started@5555');
-});
 
 
+
+
+
+
+//除了app.get、app.post这种形式外，还可以采用：app.all在这里all表示get,post等任何一种请求方式，当然也可以指定为某种特定的请求方式。
+//或者app['get']('/path', function(req,res));这种形式。
+
+//现在可以绑定和监听端口了，调用app.listen()方法，接收同样的参数，比如：
+app.listen(5555);
+console.log('Listening on port 5555');
